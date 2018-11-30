91b3057ac95fdd22bb4c3aafcd0aaf2c1fbc8e7c
Damian <idustybee@gmail.com>
2018-11-30 15:54:23 +0100

    updates
    fixed some unneeded lines and added the logo locally

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,40 @@
-var username = config.username;
-var password = config.password;
-var stationID = 'asd';
+var settings = {
+	id: parseInt(Math.random() * 100, 10)
+
+};
+
+
+if (document.cookie.replace(/(?:(?:^|.*;\s*)stationID\s*\=\s*([^;]*).*$)|^.*$/, "$1")) {
+	var cookie = document.cookie;
+	settings.stationID = document.cookie.replace(/(?:(?:^|.*;\s*)stationID\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+	console.log('cookie found!');
+	console.log('cookie: '+ cookie);
+} else {
+	
+	settings.stationID = 'asd';
+}
+
+const ws = new WebSocket("wss://idustybee.net:8443");
+
+ws.onmessage = function(a) {
+	let message = JSON.parse(a.data);
+	console.log(message);
+	if(message.type === "train" && message.id === settings.id) {
+		if(message.newStation) {
+			settings.stationID = message.newStation;
+			getTrains();
+		}
+	}
+}
+
+ws.onopen = function() {
+	toSend = {
+		type: "train",
+		id: settings.id,
+		currentStation: settings.stationID
+	}
+	ws.send(JSON.stringify(toSend));
+}
 
 $('.current-time').html(moment().format('HH:mm'));
 getTrains();
@@ -13,6 +47,24 @@ setInterval(function() {
 	$('.current-time').html(moment().format('HH:mm'));
     getTrains();
 }, 60000);
+
+
+function setStation(station) {
+	document.cookie = "stationID=" +station;
+	settings.stationID = station;
+	getTrains();
+}
+
+$(document).keydown(function(event) {
+	if(event.key == "F2") {
+		var station = window.prompt('Specify station ID\n default is Amsterdam Central Station (asd)', 'asd');
+		if (station == null || station == "") {
+			return null;
+		} else {
+			setStation(station);
+		}
+	}
+})
 
 function xmlToJson(xml) {
 	
@@ -54,15 +106,17 @@ function xmlToJson(xml) {
 
 function getTrains() {
     $.get({
-        url:'https://crossorigin.me/https://'+username+':'+password+'@webservices.ns.nl/ns-api-avt?station='+stationID,
+        url:'https://idustybee.net/trains.php?stationid='+settings.stationID,
         datatype: "xml",
         data:{},
         success:function(data, status, xhr){
             var treinData = xmlToJson(data);
             updateTrains(treinData);
         }
-    });
+	});
+	console.log("updated on " +moment().format('HH:mm:ss')+", the train types would be:");
 }
+
 
 function updateTrains(train) {
     for (i = 0; i < 10; i++) {
@@ -77,7 +131,7 @@ function updateTrains(train) {
 		console.log(train.ActueleVertrekTijden.VertrekkendeTrein[i].Vervoerder['#text']);
 
 		if(train.ActueleVertrekTijden.VertrekkendeTrein[i].Vervoerder['#text'] == "NS" || train.ActueleVertrekTijden.VertrekkendeTrein[i].Vervoerder['#text'] == "NS international") {
-			$('#logo'+i).attr('src', 'https://ns.nl/static/generic/2.21.0/images/nslogo.svg');
+			$('#logo'+i).attr('src', 'images/nslogo.svg');
 		} else if (train.ActueleVertrekTijden.VertrekkendeTrein[i].TreinSoort['#text'] == "Thalys") {
 			$('#logo'+i).attr('src', 'images/Thalys.svg');
 		} else {
@@ -114,7 +168,17 @@ function updateTrains(train) {
 		if (train.ActueleVertrekTijden.VertrekkendeTrein[i].Opmerkingen !== undefined) {
 			$('#row'+i+'-RouteTekst').html(train.ActueleVertrekTijden.VertrekkendeTrein[i].Opmerkingen.Opmerking['#text']);
 			$('#row'+i+'-RouteTekst').addClass('Comments');
-		} else {
+
+			// Work in progress --> maakt alle text in de row grijs om aan te duiden dat de trein uitvalt
+
+			// if (train.ActueleVertrekTijden.VertrekkendeTrein[i].Opmerkingen.Opmerking['#text'] === "Rijdt niet verder dan Eindhoven"){
+			// 	$('#row'+i+'-RouteTekst')
+			// 	.closest('line')
+			// 	.css("color: grey !important;");
+			// 	console.log("hmm");
+			// }
+		}
+		 else {
 			$('#row'+i+'-RouteTekst').removeClass('Comments');
 		}
         var VertrekTijdRaw = train.ActueleVertrekTijden.VertrekkendeTrein[i].VertrekTijd['#text'];
